5ade35331a5a852a5ec0e1417a2cc2e726d3d683
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2024-09-22 13:24:08 +0900

    FEAT : NaNchecker.f90
    Simple update for quadruple precision.

--- src/NaNchecker.f90
+++ src/NaNchecker.f90
@@ -11,12 +11,16 @@ module NaNchecker
         module procedure    &
           & isNaN_scalar_s, &
           & isNaN_scalar_d, &
+          & isNaN_scalar_q, &
           & isNaN_1dim_s  , &
           & isNaN_1dim_d  , &
+          & isNaN_1dim_q  , &
           & isNaN_2dim_s  , &
           & isNaN_2dim_d  , &
+          & isNaN_2dim_q  , &
           & isNaN_3dim_s  , &
-          & isNaN_3dim_d 
+          & isNaN_3dim_d  , & 
+          & isNaN_3dim_q
     end interface isNaN
 
 
@@ -41,6 +45,15 @@ module NaNchecker
     end function isNaN_scalar_d
 
 
+    function isNaN_scalar_q(input) result(output)
+        real(16), intent(in) :: input
+        logical :: output
+
+        output = IEEE_IS_NAN(input)
+
+    end function isNaN_scalar_q
+
+
     function isNaN_1dim_s(input) result(output)
         real(4), intent(in) :: input(:)
         logical :: output
@@ -57,6 +70,15 @@ module NaNchecker
         output = any(IEEE_IS_NAN(input(:)))
 
     end function isNaN_1dim_d
+
+
+    function isNaN_1dim_q(input) result(output)
+        real(16), intent(in) :: input(:)
+        logical :: output
+
+        output = any(IEEE_IS_NAN(input(:)))
+
+    end function isNaN_1dim_q
 
 
     function isNaN_2dim_s(input) result(output)
@@ -77,6 +99,15 @@ module NaNchecker
     end function isNaN_2dim_d
 
 
+    function isNaN_2dim_q(input) result(output)
+        real(16), intent(in) :: input(:,:)
+        logical :: output
+
+        output = any(IEEE_IS_NAN(input(:,:)))
+
+    end function isNaN_2dim_q
+
+
     function isNaN_3dim_s(input) result(output)
         real(4), intent(in) :: input(:,:,:)
         logical :: output
@@ -93,6 +124,15 @@ module NaNchecker
         output = any(IEEE_IS_NAN(input(:,:,:)))
 
     end function isNaN_3dim_d
+
+
+    function isNaN_3dim_q(input) result(output)
+        real(16), intent(in) :: input(:,:,:)
+        logical :: output
+
+        output = any(IEEE_IS_NAN(input(:,:,:)))
+
+    end function isNaN_3dim_q
 
 
 end module NaNchecker
